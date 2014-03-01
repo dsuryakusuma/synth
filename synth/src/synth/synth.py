0c6979f980fdb8aca41e07f0b38363ac684e39f5
zhaoallan <zhaoallan@berkeley.edu>
2014-03-01 14:19:02 -0800

    whatever

--- synth/src/synth/synth.py
+++ synth/src/synth/synth.py
@@ -1,17 +1,28 @@
 import pyaudio
+from math import sin, pi as PI
+from array import array
+import struct
+import time
 
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
+CHANNELS = 1
+SAMPLING_RATE = 44100
 
 p = pyaudio.PyAudio()
 stream = p.open(frames_per_buffer=CHUNK_SIZE,
                 format=FORMAT,
                 channels=CHANNELS,
-                rate=RATE)
+                rate=SAMPLING_RATE,
+                output=True)
 
-buffer = [0] * CHUNK_SIZE
-for i in range(buffer):
-    buffer[i] = 
-stream.write(, num_frames, exception_on_underflow)
+samples = [0] * SAMPLING_RATE
+for i in range(len(samples)):
+    samples[i] = round(sin(i / SAMPLING_RATE * PI * 2 * 880) * 64)
+
+sample_buffer = struct.pack('h' * len(samples), *samples)
+
+start = time.time()
+stream.write(sample_buffer)
+stop = time.time()
+print(stop - start)
